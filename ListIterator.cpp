59867202615099deaa2e03fd17c5c13008923b90
Laurentiu Vlaicu <vlaicul@outlook.com>
2025-03-31 01:54:28 +0300

    iterator constructor, getCurrent()

--- ListIterator.cpp
+++ ListIterator.cpp
@@ -1,25 +1,30 @@
 #include "ListIterator.h"
 #include "IndexedList.h"
 #include <exception>
+#include <stdexcept>
+#include <iostream>
+#include <assert.h>
 
-ListIterator::ListIterator(const IndexedList& list) : list(list){
+ListIterator::ListIterator(const IndexedList &list) : list(list) {
+    currentIndex = 0;
+}
+
+void ListIterator::first() {
     //TODO - Implementation
 }
 
-void ListIterator::first(){
+void ListIterator::next() {
     //TODO - Implementation
 }
 
-void ListIterator::next(){
+bool ListIterator::valid() const {
     //TODO - Implementation
+    //return false;
 }
 
-bool ListIterator::valid() const{
-    //TODO - Implementation
-    return false;
-}
-
-TElem ListIterator::getCurrent() const{
-    //TODO - Implementation
-    return NULL_TELEM;
+TElem ListIterator::getCurrent() const {
+    if (valid()) {
+        return list.elems[currentIndex];
+    }
+    throw std::out_of_range("Iterator out of range");
 }
